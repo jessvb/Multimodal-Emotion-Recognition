f7ec77e2ac74da0284c78d7b3bf3d7e9c1e2f591
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2020-11-27 14:21:58 -0800

    Analzye all files in recordings dir

--- audio_ser_from_file.py
+++ audio_ser_from_file.py
@@ -11,62 +11,58 @@ import os
 from collections import Counter
 import altair as alt
 
-# ### Flask imports
-# import requests
-# from flask import Flask, render_template, session, request, redirect, flash, Response
-
 ### Audio imports ###
 from library.speech_emotion_recognition import *
 
-# ### Video imports ###
-# from library.video_emotion_recognition import *
-
-# ### Text imports ###
-# from library.text_emotion_recognition import *
-# from library.text_preprocessor import *
-# from nltk import *
-# from tika import parser
-# from werkzeug.utils import secure_filename
-# import tempfile
-
-
 ################################################################################
-############ Change these depending on what you name the recordings ############
-input_audio = 'super_happy.wav'
-input_audio_filepath = os.path.join('recordings',input_audio)
-output_name = input_audio.split('.')[0]
+########## Change this depending on where your recordings are located ##########
+################################################################################
+rec_dir = 'recordings/'
 ################################################################################
 
+def getAudioEmotions(input_audio_filepath,output_name,SER):
+    # # Predict emotion in voice at each time step
+    step = 1 # in sec
+    sample_rate = 16000 # in kHz
+    emotions, timestamp = SER.predict_emotion_from_file(input_audio_filepath, chunk_step=step*sample_rate)
+
+    # # Export predicted emotions to .csv format
+    df = pd.DataFrame({'EMOTION': emotions, 'TIMESTAMP': timestamp})
+    df.to_csv(os.path.join('output', 'audio_emotions_' + output_name + '.csv'), sep=',', index=False)
+
+    # # Get most common emotion during the interview
+    major_emotion = max(set(emotions), key=emotions.count)
+
+    # # Calculate emotion distribution
+    emotion_dist = [int(100 * emotions.count(emotion) / len(emotions)) for emotion in SER._emotion.values()]
+
+    # # Export emotion distribution to .csv format for D3JS
+    df = pd.DataFrame(emotion_dist, index=SER._emotion.values(), columns=['VALUE']).rename_axis('EMOTION')
+    df.to_csv(os.path.join('output', 'audio_emotions_dist_' + output_name + '.csv'), sep=',')
+
+    print('🎉 Done! 🎉')
+    print('See the output files:')
+    print('output/' + 'audio_emotions_' + output_name + '.csv')
+    print('output/' + 'audio_emotions_dist_' + output_name + '.csv')
 
 
+if __name__ == '__main__':
+    # # Sub dir to speech emotion recognition model
+    model_sub_dir = os.path.join('Models', 'audio.hdf5')
+    # # Instanciate new SpeechEmotionRecognition object
+    SER = speechEmotionRecognition(model_sub_dir)
 
-# # Sub dir to speech emotion recognition model
-model_sub_dir = os.path.join('Models', 'audio.hdf5')
+    # # Loop through specific files and analyze their audio
+    files_in_dir = [f for f in os.listdir(rec_dir) if os.path.isfile(os.path.join(rec_dir, f))]
+    i = 0
+    for f in files_in_dir:
+        if f.split('.')[1] == 'm4a' or f.split('.')[1] == 'wav':
+            input_audio_filepath = os.path.join(rec_dir,f)
+            print(f'Reading from {input_audio_filepath}')
+            output_name = f.split('.')[0]
 
-# # Instanciate new SpeechEmotionRecognition object
-SER = speechEmotionRecognition(model_sub_dir)
-
-# # Predict emotion in voice at each time step
-step = 1 # in sec
-sample_rate = 16000 # in kHz
-emotions, timestamp = SER.predict_emotion_from_file(input_audio_filepath, chunk_step=step*sample_rate)
-
-# # Export predicted emotions to .csv format
-df = pd.DataFrame({'EMOTION': emotions, 'TIMESTAMP': timestamp})
-df.to_csv(os.path.join('output', output_name + '_audio_emotions.csv'), sep=',', index=False)
-# SER.prediction_to_csv(emotions, os.path.join("output", output_name + "_audio_emotions.csv"), mode='w')
-
-# # Get most common emotion during the interview
-major_emotion = max(set(emotions), key=emotions.count)
-
-# # Calculate emotion distribution
-emotion_dist = [int(100 * emotions.count(emotion) / len(emotions)) for emotion in SER._emotion.values()]
-
-# # Export emotion distribution to .csv format for D3JS
-df = pd.DataFrame(emotion_dist, index=SER._emotion.values(), columns=['VALUE']).rename_axis('EMOTION')
-df.to_csv(os.path.join('output', output_name + '_audio_emotions_dist.csv'), sep=',')
-
-print('🎉 Done! 🎉')
-print('See the output files:')
-print('output/' + output_name + '_audio_emotions.csv')
-print('output/' + output_name + '_audio_emotions_dist.csv')
+            getAudioEmotions(input_audio_filepath, output_name, SER)
+            
+        i += 1
+        print(f"""Number of files to go: {len(files_in_dir) - i}
+            Percent files done: {i/len(files_in_dir)*100}\n""")
--- video_er_from_file.py
+++ video_er_from_file.py
@@ -24,226 +24,167 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras import backend as K
 
 ################################################################################
-############ Change these depending on what you name the recordings ############
-input_video = 'avimaybe.avi'
-input_video_filepath = os.path.join('recordings',input_video)
-output_name = input_video.split('.')[0]
+########## Change this depending on where your recordings are located ##########
+################################################################################
+rec_dir = 'recordings/'
 ################################################################################
 
-# Start video capute. 0 = Webcam, 1 = Video file, -1 = Webcam for Web
-video_capture = cv2.VideoCapture(input_video_filepath)
+def getVideoEmotions(input_video_filepath,output_name):
+    # Start video capute. 0 = Webcam, 1 = Video file, -1 = Webcam for Web
+    video_capture = cv2.VideoCapture(input_video_filepath)
 
-# Image shape
-shape_x = 48
-shape_y = 48
-input_shape = (shape_x, shape_y, 1)
+    # Image shape
+    shape_x = 48
+    shape_y = 48
+    input_shape = (shape_x, shape_y, 1)
 
-# We have 7 emotions
-nClasses = 7
+    # We have 7 emotions
+    nClasses = 7
 
-# Timer until the end of the recording
-curr_frame_num = 0
+    # Timer until the end of the recording
+    curr_frame_num = 0
 
-# Count number of eye blinks (not used in model prediction)
-def eye_aspect_ratio(eye):
-    
-    A = distance.euclidean(eye[1], eye[5])
-    B = distance.euclidean(eye[2], eye[4])
-    C = distance.euclidean(eye[0], eye[3])
-    ear = (A + B) / (2.0 * C)
-    
-    return ear
+    # Initiate Landmarks
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-# Detect facial landmarks and return coordinates (not used in model prediction but in visualization)
-def detect_face(frame):
-    
-    #Cascade classifier pre-trained model
-    cascPath = 'Models/face_landmarks.dat'
-    faceCascade = cv2.CascadeClassifier(cascPath)
-    
-    #BGR -> Gray conversion
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    #Cascade MultiScale classifier
-    detected_faces = faceCascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=6,
-                                                    minSize=(shape_x, shape_y),
-                                                    flags=cv2.CASCADE_SCALE_IMAGE)
-    coord = []
-                                                    
-    for x, y, w, h in detected_faces :
-        if w > 100 :
-            # Square around the landmarks
-            sub_img=frame[y:y+h,x:x+w]
-            # Put a rectangle around the face
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0, 255,255),1)
-            coord.append([x,y,w,h])
-                                                        
-    return gray, detected_faces, coord
+    (nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
+    (mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+    (jStart, jEnd) = face_utils.FACIAL_LANDMARKS_IDXS["jaw"]
 
-#  Zoom on the face of the person
-def extract_face_features(faces, offset_coefficients=(0.075, 0.05)):
-    
-    # Each face identified
-    gray = faces[0]
-    
-    # ID of each face identifies
-    detected_face = faces[1]
-    
-    new_face = []
-    
-    for det in detected_face :
-        # Region in which the face is detected
-        # x, y represent the starting point, w the width (moving right) and h the height (moving up)
-        x, y, w, h = det
+    (eblStart, eblEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
+    (ebrStart, ebrEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
+
+    # Load the pre-trained X-Ception model
+    model = load_model('Models/video.h5')
+
+    # Load the face detector
+    face_detect = dlib.get_frontal_face_detector()
+
+    # Load the facial landmarks predictor
+    predictor_landmarks  = dlib.shape_predictor("Models/face_landmarks.dat")
+
+    # Prediction vector
+    predictions = []
+
+    # Timer for length of video
+    max_frame_num = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = video_capture.get(cv2.CAP_PROP_FPS)
+    frames_btwn = int(fps/3) # measure emotion every 1/3 of a sec
+
+    # angry_0 = []
+    # disgust_1 = []
+    # fear_2 = []
+    # happy_3 = []
+    # sad_4 = []
+    # surprise_5 = []
+    # neutral_6 = []
+
+    # Initialize arrays for saving predictions
+    emotions = []
+    face_indices = []
+    timestamps = []
+
+    # Analyze video until the end
+    curr_frame_num = 0
+    iter_percent = 0 # for printing
+    while curr_frame_num < max_frame_num:    
+        # Set the frame to be read
+        video_capture.set(cv2.CAP_PROP_POS_FRAMES, curr_frame_num)
         
-        #Offset coefficient (margins), np.floor takes the lowest integer (delete border of the image)
-        horizontal_offset = np.int(np.floor(offset_coefficients[0] * w))
-        vertical_offset = np.int(np.floor(offset_coefficients[1] * h))
+        # Capture the frame number set above (frame here means image)
+        ret, frame = video_capture.read()
         
-        # Coordinates of the extracted face
-        extracted_face = gray[y+vertical_offset:y+h, x+horizontal_offset:x-horizontal_offset+w]
+        # Face index, face by face
+        face_index = 0
         
-        #Zoom on the extracted face
-        new_extracted_face = zoom(extracted_face, (shape_x / extracted_face.shape[0],shape_y / extracted_face.shape[1]))
+        # Image to gray scale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-        # Cast type to float
-        new_extracted_face = new_extracted_face.astype(np.float32)
+        # All faces detected
+        rects = face_detect(gray, 1)
         
-        # Scale the new image
-        new_extracted_face /= float(new_extracted_face.max())
-        
-        # Append the face to the list
-        new_face.append(new_extracted_face)
-    
-    return new_face
+        # For each detected face
+        for (i, rect) in enumerate(rects):
+            # Identify face coordinates
+            (x, y, w, h) = face_utils.rect_to_bb(rect)
+            face = gray[y:y+h,x:x+w]
 
-# Initiate Landmarks
-(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+            # Zoom on extracted face (if face extracted)
+            if face.shape[0] and face.shape[1]:
+                face = zoom(face, (shape_x / face.shape[0],shape_y / face.shape[1]))
 
-(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
-(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
-(jStart, jEnd) = face_utils.FACIAL_LANDMARKS_IDXS["jaw"]
+                # Cast type float
+                face = face.astype(np.float32)
 
-(eblStart, eblEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eyebrow"]
-(ebrStart, ebrEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eyebrow"]
+                # Scale the face
+                face /= float(face.max())
+                face = np.reshape(face.flatten(), (1, 48, 48, 1))
 
-# Load the pre-trained X-Ception model
-model = load_model('Models/video.h5')
+                # Make Emotion prediction on the face, outputs probabilities
+                prediction = model.predict(face)
 
-# Load the face detector
-face_detect = dlib.get_frontal_face_detector()
+                # Most likely emotion (as an index)
+                prediction_result = np.argmax(prediction)
 
-# Load the facial landmarks predictor
-predictor_landmarks  = dlib.shape_predictor("Models/face_landmarks.dat")
+                # Convert emotion index to an emotion (string)
+                emotion = ''
+                if (prediction_result == 0):
+                    emotion = 'Angry'
+                elif (prediction_result == 1):
+                    emotion = 'Disgust'
+                elif (prediction_result == 2):
+                    emotion = 'Fear'
+                elif (prediction_result == 3):
+                    emotion = 'Happy'
+                elif (prediction_result == 4):
+                    emotion = 'Sad'
+                elif (prediction_result == 5):
+                    emotion = 'Surprise'
+                elif (prediction_result == 6):
+                    emotion = 'Neutral'
+                else:
+                    emotion = 'Unknown emotion'
+            else:
+                emotion = 'No face extracted'
+            # save results for later
+            emotions.append(emotion)
+            face_indices.append(face_index)
+            timestamps.append(curr_frame_num/fps)
 
-# Prediction vector
-predictions = []
+        # every so often, show percent done
+        percent_done = curr_frame_num/max_frame_num*100
+        if (percent_done > iter_percent):
+            print('current frame: %.0f' % curr_frame_num)
+            print('percent done: %.1f%%' % percent_done)
+            iter_percent += 20
 
-# Timer for length of video
-max_frame_num = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
-fps = video_capture.get(cv2.CAP_PROP_FPS)
-frames_btwn = int(fps/3) # measure emotion every 1/3 of a sec
+        # increment frame
+        curr_frame_num += frames_btwn
 
-angry_0 = []
-disgust_1 = []
-fear_2 = []
-happy_3 = []
-sad_4 = []
-surprise_5 = []
-neutral_6 = []
+    video_capture.release()
 
-# Initialize arrays for saving predictions
-emotions = []
-face_indices = []
-timestamps = []
+    # # Export predicted emotions to .csv format
+    df = pd.DataFrame({'EMOTION': emotions, 'FACE_INDEX': face_indices, 'TIMESTAMP_SEC': timestamps})
+    df.to_csv(os.path.join('output', 'video_emotions_' + output_name + '.csv'), sep=',', index=False)
 
-# Analyze video until the end
-curr_frame_num = 0
-iter_percent = 0 # for printing
-while curr_frame_num < max_frame_num:    
-    # Set the frame to be read
-    video_capture.set(cv2.CAP_PROP_POS_FRAMES, curr_frame_num)
-    
-    # Capture the frame number set above (frame here means image)
-    ret, frame = video_capture.read()
-    
-    # Face index, face by face
-    face_index = 0
-    
-    # Image to gray scale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    # All faces detected
-    rects = face_detect(gray, 1)
-    
-    # For each detected face
-    for (i, rect) in enumerate(rects):
-        
-        # Identify face coordinates
-        (x, y, w, h) = face_utils.rect_to_bb(rect)
-        face = gray[y:y+h,x:x+w]
-        
-        # Identify landmarks and cast to numpy
-        shape = predictor_landmarks(gray, rect)
-        shape = face_utils.shape_to_np(shape)
-        
-        # Zoom on extracted face
-        face = zoom(face, (shape_x / face.shape[0],shape_y / face.shape[1]))
-        
-        # Cast type float
-        face = face.astype(np.float32)
-        
-        # Scale the face
-        face /= float(face.max())
-        face = np.reshape(face.flatten(), (1, 48, 48, 1))
-        
-        # Make Emotion prediction on the face, outputs probabilities
-        prediction = model.predict(face)
-        
-        # Most likely emotion (as an index)
-        prediction_result = np.argmax(prediction)
+    print('🎉 Done! 🎉')
+    print('See the output file:')
+    print('output/' + 'video_emotions_' + output_name + '.csv')
 
-        # Convert emotion index to an emotion (string)
-        emotion = ''
-        if (prediction_result == 0):
-            emotion = 'Angry'
-        elif (prediction_result == 1):
-            emotion = 'Disgust'
-        elif (prediction_result == 2):
-            emotion = 'Fear'
-        elif (prediction_result == 3):
-            emotion = 'Happy'
-        elif (prediction_result == 4):
-            emotion = 'Sad'
-        elif (prediction_result == 5):
-            emotion = 'Surprise'
-        elif (prediction_result == 6):
-            emotion = 'Neutral'
-        else:
-            emotion = 'Unknown emotion'
-        
-        # save results for later
-        emotions.append(emotion)
-        face_indices.append(face_index)
-        timestamps.append(curr_frame_num/fps)
 
-    # every so often, show percent done
-    percent_done = curr_frame_num/max_frame_num*100
-    if (percent_done > iter_percent):
-        print('current frame: %.0f' % curr_frame_num)
-        print('percent done: %.1f%%' % percent_done)
-        iter_percent += 20
+if __name__ == '__main__':
+    # Loop through specific files and analyze their video
+    files_in_dir = [f for f in os.listdir(rec_dir) if os.path.isfile(os.path.join(rec_dir, f))]
+    i = 0
+    for f in files_in_dir:
+        if f.split('.')[1] == 'avi' or f.split('.')[1] == 'mp4':
+            input_video_filepath = os.path.join(rec_dir,f)
+            print(f'Reading from {input_video_filepath}')
+            output_name = f.split('.')[0]
 
-    # increment frame
-    curr_frame_num += frames_btwn
+            getVideoEmotions(input_video_filepath, output_name)
 
-video_capture.release()
-
-# # Export predicted emotions to .csv format
-df = pd.DataFrame({'EMOTION': emotions, 'FACE_INDEX': face_indices, 'TIMESTAMP_SEC': timestamps})
-df.to_csv(os.path.join('output', output_name + '_video_emotions.csv'), sep=',', index=False)
-
-print('🎉 Done! 🎉')
-print('See the output file:')
-print('output/' + output_name + '_video_emotions.csv')
+        i += 1
+        print(f"""Number of files to go: {len(files_in_dir) - i}
+            Percent files done: {i/len(files_in_dir)*100}\n""")
